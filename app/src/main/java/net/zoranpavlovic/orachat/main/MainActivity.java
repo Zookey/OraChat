d10ba658360458c9f9a95814f5b3f37f4c137221
Zoran Pavlovic <zoranp90@gmail.com>
2017-07-29 14:11:05 +0200

    Working on bottom navigation menu for main screen.

--- app/src/main/java/net/zoranpavlovic/orachat/main/MainActivity.java
+++ app/src/main/java/net/zoranpavlovic/orachat/main/MainActivity.java
@@ -1,15 +1,77 @@
 package net.zoranpavlovic.orachat.main;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import net.zoranpavlovic.orachat.R;
+import net.zoranpavlovic.orachat.account.AccountFragment;
+import net.zoranpavlovic.orachat.chats.ChatsFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.bottom_navigation) BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        setUpBottomNavigationMenu();
+
     }
+
+    private void setUpBottomNavigationMenu() {
+        selectFragment(bottomNavigationView.getMenu().getItem(0));
+        bottomNavigationListener();
+    }
+
+    private void bottomNavigationListener() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectFragment(item);
+                return true;
+            }
+        });
+    }
+
+    private void selectFragment(@NonNull MenuItem item) {
+        item.setChecked(true);
+        switch (item.getItemId()){
+            case R.id.action_chats:
+                replaceFragment(new ChatsFragment());
+                break;
+            case R.id.action_account:
+                replaceFragment(new AccountFragment());
+                break;
+        }
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        if (fragment == null) {
+            return;
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        if (fragmentManager != null) {
+            FragmentTransaction ft = fragmentManager.beginTransaction();
+            if (ft != null) {
+                ft.replace(R.id.frame_layout, fragment);
+                ft.commit();
+            }
+        }
+    }
+
 }
